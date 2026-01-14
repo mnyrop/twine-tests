ebfcae527016d3c6e6611779b122b5cec8a256b1
mnyrop <marii@nyu.edu>
2026-01-14 16:55:10 -0500

    switch to sugarcube, generate index

--- build.js
+++ build.js
@@ -3,11 +3,33 @@ const fs        = require('fs');
 const glob      = require('glob');
 var path        = require('path');
 
-const format = 'harlowe/3.3.9';
-const files  = glob.sync("src/stories/**/*.twee");
+const format    = 'sugarcube/2.37.3';
+const files     = glob.sync("src/stories/**/*.twee");
+const indexHtml = `
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Story Index</title>
+  </head>
+  <body>
+    <h1>Index</h1>
+    <ul>
+      ${files.map(file => {
+        const target = file.replace("src/stories/", "").replace(".twee", ".html");
+        return `<li><a href="${target}">${target}</a></li>`;
+      }).join('\n      ')}
+    </ul>
+  </body>
+  </html>
+`;
+
 
 console.log(`Building stories with ${format}...\n`);
 fs.rmSync("dist", { recursive: true, force: true });
+fs.mkdirSync("dist", { recursive: true });
+fs.writeFileSync("dist/index.html", indexHtml); 
 
 for (const file of files) {
   const target = file.replace("src/stories/", "dist/").replace(".twee", ".html");
